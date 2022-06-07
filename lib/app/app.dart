3b4984db328e1bf99b8cd96fd9cec3a8b537ec07
ahmedalkayyal <ahmedalkayyal92@gmail.com>
2022-06-07 14:16:06 +0300

    Initial commit

---
+++ lib/app/app.dart
@@ -0,0 +1,26 @@
+import 'package:advanced_flutter_arabic/presentation/resources/theme_manager.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatefulWidget {
+  // named constructor
+  MyApp._internal();
+
+  int appState = 0;
+
+  static final MyApp _instance =
+      MyApp._internal(); // singleton or single instance
+
+  factory MyApp() => _instance; // factory
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: getApplicationTheme(),
+    );
+  }
+}
